1a951a6f9f4e6718e294321b40b990fd4aa76ea0
NKempf <kempfnicolas@gmail.com>
2019-02-15 16:22:27 -0400

    arborescence

---
+++ Other programs/3 Documentation/2 Gantt Chart/Agate - Gantt Chart app.R
@@ -0,0 +1,195 @@
+#--------------------------------------------------------------------------------------------------------------------------------#
+#                      Agate - Gantt chart app                                                                                   #
+#--------------------------------------------------------------------------------------------------------------------------------#
+
+# MAJ : 30.08.2018
+
+# Nicolas Kempf
+
+# Application RShiny de creation, modification et affichage du diagramme de gantt du projet Agate
+
+# https://github.com/daattali/timevis
+# https://www.w3schools.com/css/css_border.asp
+# https://clrs.cc/
+
+# Packages nécessaires
+#---------------------
+library(timevis) # Interactive Gantt chart
+library(tidyverse) # transformation des données et calcul de statistiques descriptives simples.
+library(shiny) # Interface graphique
+library(shinyWidgets) # Widgets supplementaires
+
+# Data
+load("Data/Gantt/gantt.RData")
+
+groups <- data.frame(
+  id = c("prot","app","miniApp"),
+  content = c("Prototype","Agate","Mini Agate")
+)
+
+
+templateNK <- function(tache, equipe, pourcent) {
+  sprintf(    
+    '<table><tbody>
+    <tr><td colspan="3"><b>%s</b></td></tr>
+    <tr><td><i>%s</i></td></tr>
+    <tr><td>%s</td></tr>
+    </tbody></table>',
+    tache, equipe,pourcent 
+  )
+}
+
+
+
+# I. Interface graphique
+#---------------------------------------------------------------------------------------------------------------------------------------
+ui <- fluidPage(
+  title = "Agate gantt",
+  
+  fluidRow(
+    column(
+      8,
+      fluidRow(column(12,
+                      timevisOutput("timelineInteractive")
+      )),
+      # fluidRow(
+      #   column(
+      #     12,
+      #     div(id = "interactiveActions",
+      #         class = "optionsSection",
+      #         tags$h4("Actions:"),
+      #         actionButton("fit", "Fit all items"),
+      #         actionButton("setWindowAnim", "Set window 2016-01-07 to 2016-01-25"),
+      #         actionButton("setWindowNoAnim", "Set window without animation"),
+      #         actionButton("center", "Center around 2016-01-23"),
+      #         actionButton("focus2", "Focus item 4"),
+      #         actionButton("focusSelection", "Focus current selection"),
+      #         actionButton("addTime", "Add a draggable vertical bar 2016-01-17")
+      #     )
+      #   )
+      # ),
+      fluidRow(
+        column(
+          4,
+          div(class = "optionsSection",
+              selectInput("AddGroup",label = "Selectionner le groupe",choices = groups$content),
+              textInput("addTache", tags$h4("Ajouter une tache"), "Nouvelle tache"),
+              textInput("addEquipe", tags$h4("Ajouter participant"), "Hypolite")
+              
+              # dateInput("startDate", label = "Date de début", Sys.Date()),
+              # dateInput("endDate", label = "Date de fin", Sys.Date()),
+              # actionButton("addBtn", "Ajouter")
+          )
+        ),
+        column(
+          4,
+          div(class = "optionsSection",
+              textInput("addPourcent", tags$h4("Pourcentage"), "%"),
+              dateInput("startDate", label = "Date de début", Sys.Date()),
+              dateInput("endDate", label = "Date de fin", Sys.Date()),
+              fluidRow(
+                actionButton("addBtn", "Ajouter"),
+                actionButton("addSave", "Sauvegarder")
+                
+              )
+              
+          )
+        )
+      )
+    ),
+    column(4,
+           div(
+             id = "timelinedata",
+             class = "optionsSection",
+             tags$h4("Data:"),
+             tableOutput("table"),
+             hr(),
+             div(tags$strong("Visible window:"),
+                 textOutput("window", inline = TRUE)),
+             div(tags$strong("Selected items:"),
+                 textOutput("selected", inline = TRUE))
+           )
+    ),
+    
+    useSweetAlert()
+  )
+)
+
+
+# II. Server
+#--------------------------------------------------------------------------------------------------------------------------------------------------
+server <- function(input, output, session) {
+  
+  # Affichage de la timeline
+  output$timelineInteractive <- renderTimevis({
+    config <- list(
+      editable = TRUE,
+      multiselect = TRUE
+    )
+    timevis(df,
+            groups = groups,
+            options = config
+    )
+  })
+  
+  # Affichage du tableau
+  output$table <- renderTable({
+    data <- input$timelineInteractive_data
+    data$start <- prettyDate(data$start)
+    if(!is.null(data$end)) {
+      data$end <- prettyDate(data$end)
+    }
+    data[,c("id","tache","start","end","group")]
+  })
+  
+  # Ajout d'une tache
+  observeEvent(input$addBtn, {
+    addItem("timelineInteractive",
+            data = list(id = randomID(),
+                        tache = input$addTache,
+                        team = input$addEquipe,
+                        pct = input$addPourcent,
+                        content = templateNK(input$addTache,input$addEquipe,paste0(input$addPourcent," %")),
+                        start = input$startDate,
+                        end = input$endDate,
+                        group = groups$id[groups$content == input$AddGroup],
+                        style = case_when(input$addPourcent >=75 ~ "color:#001f3f;border-style:solid;background-color:#2ECC40;",
+                                          input$addPourcent < 25 ~ "color:#001f3f;border-style:solid;background-color:#FF4136;",
+                                          TRUE ~ "color:#001f3f;border-style:solid;background-color:#FFDC00;")))
+  })
+  
+  # Sauvegarde de la table
+  observeEvent(input$addSave, {
+    
+    
+    df <- input$timelineInteractive_data
+    save(df,groups,file="Data/Gantt/gantt.RData")
+    
+    
+    shinyWidgets::sendSweetAlert(
+      session = session, 
+      title = "Succes", text = "Table sauvegardée !", type = "success"
+    )
+    
+    
+  })
+  
+}
+
+
+
+# generate a random string of 16 characters
+randomID <- function() {
+  paste(sample(c(letters, LETTERS, 0:9), 16, replace = TRUE), collapse = "")
+}
+
+prettyDate <- function(d) {
+  suppressWarnings(format(as.POSIXct(gsub("T", " ", d), "%Y-%m-%d %H:%M")))
+}
+
+
+
+# III. Application
+#---------------------------------------------------------------------------------------------------------------------------------------------------
+shinyApp(ui = ui, server = server)
+
---
+++ Other programs/3 Documentation/2 Gantt Chart/Agate - Gantt Chart.R
@@ -0,0 +1,61 @@
+#--------------------------------------------------------------------------------------------------------------------------------#
+#                      Agate - Gantt chart prototype                                                                             #
+#--------------------------------------------------------------------------------------------------------------------------------#
+
+# MAJ : 30.08.2018
+
+# Nicolas Kempf
+
+# Creation et affichage du diagramme de gantt du projet Agate
+
+# Packages nécessaires
+#---------------------
+library(timevis) # Interactive Gantt chart
+library(tidyverse) # transformation des données et calcul de statistiques descriptives simples.
+
+# I. Saisie des données
+#-------------------------------------------------------------------------------------------------------------------------------
+
+# I.1. Mise en forme du contenu de chaque item
+#---------------------------------------------
+templateNK <- function(tache, equipe, pourcent) {
+  sprintf(    
+    '<table><tbody>
+      <tr><td colspan="3"><b>%s</b></td></tr>
+      <tr><td><i>%s</i></td></tr>
+      <tr><td>%s</td></tr>
+    </tbody></table>',
+    tache, equipe,pourcent 
+  )
+}
+
+# I.2. Grandes familles de tâches
+#--------------------------------
+groups <- data.frame(
+  id = c("prot","app","miniApp"),
+  content = c("Prototype","Agate","Mini Agate")
+)
+
+# I.3. Liste des tâches
+#----------------------
+df <- data.frame(id=1:3,
+                 tache = c("Prototype Agate","Déploiement application","Qualité du RP"),
+                 team = c("Nico et Baptiste","Baptiste","Nico et Baptiste"),
+                 pct = c(90,24,45),
+                 start = c("2018-08-29","2018-08-30","2018-09-06"),
+                 end = c("2018-08-30","2018-09-01","2018-09-10"),
+                 group = c("prot","prot","prot"),
+                 type = c("range","range","range")) %>% 
+  mutate(content = templateNK(tache = tache,equipe = team,pourcent = paste0(pct," %")),
+         style = case_when(pct >=75 ~ "color:#001f3f;border-style:solid;background-color:#2ECC40;",
+                           pct < 25 ~ "color:#001f3f;border-style:solid;background-color:#FF4136;",
+                           TRUE ~ "color:#001f3f;border-style:solid;background-color:#FFDC00;"))
+
+# II. Diagramme de Gantt
+#---------------------------------------------------------------------------------------------------------------------------------
+timevis(data = df, groups = groups, options = list(editable = TRUE))
+
+# III. Enregistrement de la base
+#---------------------------------------------------------------------------------------------------------------------------------
+save(groups,df,file="Data/Gantt/gantt.RData")
+
